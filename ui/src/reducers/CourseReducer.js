7636b973de31e55012c69c5621e21f81c2e43fa1
Mazhar-4321 <syedmazharali742@gmail.com>
2023-04-28 16:34:32 +0530

    [Mazhar]MODIFIED: Course Reducer File

--- ui/src/reducers/CourseReducer.js
+++ ui/src/reducers/CourseReducer.js
@@ -7,6 +7,7 @@ const initialState = {
  
  }
  const CourseReducer = ((state = initialState, action) => {
+    console.log(action.type)
      switch (action.type) {
  
          case 'refreshCourses':
@@ -26,11 +27,20 @@ const initialState = {
              return{
                  ...state,coursesEnrolled:courses
              }
+            case 'updateToken':
+                var token=action.value+""
+                console.log(token)
+                var tokenArray=token.split(",")
+                console.log(tokenArray)
+                return{
+                    ...state,token:tokenArray[1],userRole:tokenArray[0]
+                }
          
  
  
  
          default:
+            console.log("default executed")
              return state
  
  

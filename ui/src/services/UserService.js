e77e887f13932ab90d233e630577ad1ed9a0ac91
Mazhar-4321 <syedmazharali742@gmail.com>
2023-04-28 11:00:23 +0530

    [Mazhar]MODIFIED: User Service

--- ui/src/services/UserService.js
+++ ui/src/services/UserService.js
@@ -1,25 +1,32 @@
 import axios from "axios";
-const baseURL = 'http://localhost:3008/api/v1'
-const headerConfig = {
-    headers: {
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
-    }
-}
+const baseURL = 'http://localhost:3005/api/v1'
+
 
 export const registerUser = async (obj) => {
     try {
-        var response = await axios.post(`${baseURL}/users`, obj)
+        var response = await axios.post(`${baseURL}/users/`, obj)
         return response
     } catch (err) {
         throw new Error('Could Not Create User.')
     }
 }
-
 export const validateEmail = async (obj) => {
+    console.log("onjnjnj",obj);
     try {
         var response = await axios.post(`${baseURL}/users/validateEmail`, obj)
         return response
     } catch (err) {
         throw new Error('Email Sending Failed.')
+    }
+}
+
+export const login=async(obj)=>{
+    try {
+        var response = await axios.post(`${baseURL}/users/login`, obj)
+        console.log(response)
+        localStorage.setItem('token', response.data.token)
+        return response
+    } catch (err) {
+        throw new Error('Could Not Create User')
     }
 }

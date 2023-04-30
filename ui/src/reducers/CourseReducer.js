d3792e3b4ed9c4aa330623f6bcf892c18bfc000f
Mazhar-4321 <syedmazharali742@gmail.com>
2023-04-30 07:51:31 +0530

    [Mazhar]REFACTORED: Course Reducer

--- ui/src/reducers/CourseReducer.js
+++ ui/src/reducers/CourseReducer.js
@@ -1,51 +1,67 @@
 const initialState = {
-    token:null,
-    userRole:'student',
-    studentChoiceSelected:'null',
-    coursesEnrolled:[],
- 
- 
- }
- const CourseReducer = ((state = initialState, action) => {
+    token: null,
+    userRole: 'student',
+    studentChoiceSelected: 'null',
+    coursesEnrolled: new Map(),
+    userDetails: { firstName: null, lastName: null, email: null },
+    testDetails: []
+
+
+}
+const CourseReducer = ((state = initialState, action) => {
     console.log(action.type)
-     switch (action.type) {
- 
-         case 'refreshCourses':
-             return null
-         
-         case 'changeLoginStatus':
-             return{
-                 ...state,isLoggedIn:false
-             }
-         case 'changeStudentChoice':
-             var choice=action.value
-             return{
-               ...state,studentChoiceSelected:choice
-             }
-         case 'changeEnrolledCourses':
-             var courses=action.value
-             return{
-                 ...state,coursesEnrolled:courses
-             }
-            case 'updateToken':
-                var token=action.value+""
-                console.log(token)
-                var tokenArray=token.split(",")
-                console.log(tokenArray)
-                return{
-                    ...state,token:tokenArray[1],userRole:tokenArray[0]
+    switch (action.type) {
+
+        case 'updateStudentCourses':
+            var data = action.value
+            //myCourses
+            console.log("all details of student", data);
+            var map = state.coursesEnrolled;
+            data.myCourses.forEach(e => map.set(e.c_id, e))
+            return {
+                ...state, coursesEnrolled: map
+            }
+
+        case 'changeLoginStatus':
+            return {
+                ...state, isLoggedIn: false
+            }
+        case 'changeStudentChoice':
+            var choice = action.value
+            return {
+                ...state, studentChoiceSelected: choice
+            }
+        case 'changeEnrolledCourses':
+            var courses = action.value
+            return {
+                ...state, coursesEnrolled: courses
+            }
+        case 'updateToken':
+            var token = action.value
+            var tokenArray = token.split(",")
+            console.log("miya ....", tokenArray)
+            if (tokenArray[0] === 'student') {
+                return {
+                    ...state, token: tokenArray[4], userRole: tokenArray[0], userDetails: { firstName: tokenArray[1], lastName: tokenArray[2], email: tokenArray[3] }
                 }
-         
- 
- 
- 
-         default:
+            } else
+                return {
+                    ...state, token: tokenArray[2], userRole: tokenArray[0], userDetails: { email: tokenArray[1] }
+                }
+        case 'updateTestDetails':
+            return null;
+
+
+
+
+
+        default:
             console.log("default executed")
-             return state
- 
- 
-     }
- 
- }
- )
- export default CourseReducer
+            return state
+
+
+    }
+
+}
+)
+export default CourseReducer

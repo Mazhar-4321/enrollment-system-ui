8ec0becd3709605c5e0b62ac118f87a86a9d9a51
Mazhar-4321 <syedmazharali742@gmail.com>
2023-04-30 07:54:43 +0530

    [Mazhar]ADDED: CourseDetail.jsx To The Project

--- ui/src/pages/CourseDetail.jsx
+++ ui/src/pages/CourseDetail.jsx
@@ -1,15 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import IconButton from "@mui/material/IconButton";
 import Rotate from "react-reveal/Rotate";
 import "../css/CourseDetail.css";
 import dummyimg from "../images/eour.jpeg";
-
+import { useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import { enrollInTheCourse } from "../services/StudentService";
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
 
 export function CourseDetail() {
-
+  let location = useLocation();
+  const myState = useSelector(state => state.CourseReducer)
+  const [snackbar, setSnackbar] = useState(false)
+  const dispatch = useDispatch();
+  const [enroll, setEnroll] = useState(false)
+  const onSnackbarClose = () => {
+    setSnackbar(false)
+  }
+  const onAlertClose = () => {
+    setSnackbar(false)
+  }
+  const handleEnrollment = async () => {
+    console.log(myState.coursesEnrolled)
+    try {
+      var courseResponse = await enrollInTheCourse(location.state.id);
+      if (courseResponse) {
+        setEnroll(true)
+        setSnackbar(true)
+        dispatch({
+          type: 'updateCourseEnrollment',
+          value: location.state.id
+        })
+      }
+      console.log(courseResponse)
+    } catch (err) {
+      console.log(err)
+      alert("Course Can't be Registered")
+    }
+  }
   return (
     <div>
-
+      <Snackbar open={snackbar} autoHideDuration={1000} onClose={onSnackbarClose}>
+        <Alert onClose={onAlertClose} severity='success' sx={{ width: '100%' }}>
+          {'Course Enrolled SuccessFully'}
+        </Alert>
+      </Snackbar>
       <div className="originForCourseDetails">
         <Rotate top left>
           <div className="main-bookcontainer">
@@ -18,36 +57,29 @@ export function CourseDetail() {
                 <img src={dummyimg} alt="img" />
               </div>
               <div className="book-button">
-                <IconButton>Enroll Course</IconButton>
+                <IconButton disabled={enroll} onClick={handleEnrollment}>Enroll Course</IconButton>
               </div>
             </div>
             <div className="right-content">
-              <div className="title">Course Name </div>
-              <div className="auther">by Instructor</div>
+              <div className="title">{location.state.courseName} </div>
+              <div className="auther">{location.state.instructor}</div>
               <div className="pricebook">
-                <div className="dis-price">Rs. {"342"} </div>
-                <div className="real-price">Rs. {"870"}</div>
+                <div className="dis-price"> {location.state.lastDate}</div>
+                <div className="real-price">{location.state.duration}</div>
               </div>
               <hr />
               <div className="book-description">
                 <div className="detail">Course Detail </div>
                 <p className="descrip">
-                  {" "}
-                  Apple CEO Tim Cook warmly greeted the eager customers gathered
-                  Despite the smaller size, the Apple Saket store offers all the
-                  expected services like any other Apple store. Apple Saket
-                  features a uniquely designed curved storefront with white oak
-                  tables displaying Apple's products and accessories, as well as
-                  a feature wall manufactured in India outside Apple’s Delhi
-                  store with a smile He also interacted with the customers and
-                  took pictures with fans.
+                  {location.state.courseDescription}
+
                 </p>
               </div>
             </div>
           </div>
         </Rotate>
       </div>
-      
+
     </div>
   );
 }

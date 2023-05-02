273b7f27a6396d6487627747d3fdefdbbfc1f984
Mazhar-4321 <syedmazharali742@gmail.com>
2023-05-02 10:19:44 +0530

    [Mazhar]ADDED: AdminDashboard.jsx

--- ui/src/pages/AdminDashBoard.jsx
+++ ui/src/pages/AdminDashBoard.jsx
@@ -8,40 +8,49 @@ import { UploadCourse2 } from './UploadCourse2';
 import { RemoveCourse } from '../components/RemoveCourse';
 import QuestionForm from './UploadQuestion';
 import { useNavigate } from "react-router-dom";
-import { useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import ApproveCourseCertificateRequest from '../components/ApproveCourseCertificateRequest';
+import DashBoard from './DashBoard';
+import { getDashboardData } from '../services/AdminService';
 
 
 
 export default function LabTabs() {
   const [value, setValue] = React.useState('1');
+  const [visualData, setVisualData] = React.useState([])
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
+  React.useEffect(() => {
+    const dbCall = async () => {
+      var response = await getDashboardData();
+      setVisualData(response)
+    }
+    dbCall();
+  }, [])
   const handleChange = (event, newValue) => {
-    if(newValue==5){
+    if (newValue == 5) {
       dispatch({
-        type:'removeToken'
-    })
-    navigate("/");
+        type: 'removeToken'
+      })
+      navigate("/");
     }
     setValue(newValue);
   };
 
   return (
     <Box sx={{ width: '100vw', typography: 'body1' }}>
-      <TabContext  value={value}>
+      <TabContext value={value}>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <TabList centered flexContainer sx={{ width: '100vw', typography: 'body1' }} onChange={handleChange} aria-label="lab API tabs example">
             <Tab label="Dashboard" value="1" />
-            <Tab label="Upload Course" value="2" />
+            <Tab label="Upload/Edit Course" value="2" />
             <Tab label="Remove Course" value="3" />
             <Tab label="Approve Course Certificate Requests" value="4" />
             <Tab label="Logout" value="5" />
             <Tab label="Upload Quiz" value="6" />
           </TabList>
         </Box>
-        <TabPanel value="1">Dashboard</TabPanel>
+        <TabPanel value="1"><DashBoard  visualData={visualData}/></TabPanel>
         <TabPanel value="2"><UploadCourse2 /></TabPanel>
         <TabPanel value="3"><RemoveCourse /></TabPanel>
         <TabPanel value="4"><ApproveCourseCertificateRequest /></TabPanel>
